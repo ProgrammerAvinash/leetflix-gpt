1469a96355bd4f155793599386f7adca938af7fe
Avinash Yadav <62152948+ProgrammerAvinash@users.noreply.github.com>
2025-01-17 06:50:26 +0530

    init

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App text-xl bg-gray-400">
-      <h1 className="">App</h1>
+      <h1 className="">leetflix-gpt</h1>
     </div>
   );
 }
